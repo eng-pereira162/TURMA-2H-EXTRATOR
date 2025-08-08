a1b393a56c106170b63f30dee190f364d024c682
Lucas Pereira de Campos <eng.lucas.campos@usp.br>
2025-08-08 08:00:27 -0300

    corrigi o felipe

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const botaoExtrair = document.querySelector('#botao-palavrachave');
 
 botaoExtrair.addEventListener("click", Felipe);
 
-function Felipe {
-    console.log ("O Felipe vai aprender a fazer algo!")
+function Felipe () {
+    console.log ("O Felipe vai aprender a fazer algo!");
     alert("Aprendi!")
 
 }
