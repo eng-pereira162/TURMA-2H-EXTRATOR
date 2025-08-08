b7d75477ad6dc9742dd763beaaf61f855b085cad
Lucas Pereira de Campos <eng.lucas.campos@usp.br>
2025-08-08 08:05:18 -0300

    teste2

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const botaoExtrair = document.querySelector("#botao-palavrachave");
 
 botaoExtrair.addEventListener("click", Felipe);
 
-function Felipe () {
+function Felipe() { 
     console.log ("O Felipe vai aprender a fazer algo!");
-    alert("Aprendi!")
-
-}
+  alert("fui clicado!"); 
+  
+} 
